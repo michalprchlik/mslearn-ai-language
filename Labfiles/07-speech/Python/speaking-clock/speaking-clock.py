617613498900a0c8cd9479d4e952f69a10334ef7
Michal Prchlik <michal.prchlik@kyndryl.com>
2025-06-09 17:05:54 +0200

    done labs

--- Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
+++ Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import os
 
 # Import namespaces
-
+import azure.cognitiveservices.speech as speech_sdk
+from playsound import playsound
 
 def main():
     try:
@@ -15,8 +16,9 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
+        print('Ready to use speech service in:', speech_config.region)
         
-
         # Get spoken input
         command = TranscribeCommand()
         if command.lower() == 'what time is it?':
@@ -28,11 +30,36 @@ def main():
 def TranscribeCommand():
     command = ''
 
-    # Configure speech recognition
+    # Microphone
+    # audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    # speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    # print('Speak now...')
 
+    # play audio
+    current_dir = os.getcwd()
+    audioFile = current_dir + '\\time.wav'
+    playsound(audioFile)
+    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+
+    # nastaveni francouzstiny jako standardniho jazyka
+    # speech_config = speech_sdk.SpeechConfig(subscription="YOUR_SUBSCRIPTION_KEY", region="YOUR_REGION")
+    # speech_config.speech_recognition_language = "fr-FR"
+
+    # speech_recognizer = speech_sdk.SpeechRecognizer(speech_config=speech_config)
+
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
 
     # Process speech input
-
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
 
     # Return the command
     return command
@@ -42,12 +69,28 @@ def TellTime():
     now = datetime.now()
     response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
 
-
     # Configure speech synthesis
+    #speech_config.speech_synthesis_voice_name = 'en-GB-LibbyNeural'
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
     
-
     # Synthesize spoken output
+    speak = speech_synthesizer.speak_text_async(response_text).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
+    # Speech Synthesis Markup Language (SSML) enables you to customize the way your speech is synthesized using an XML-based format.
+    responseSsml = " \
+        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
+            <voice name='en-GB-LibbyNeural'> \
+                {} \
+                <break strength='weak'/> \
+                Time to end this lab! \
+            </voice> \
+        </speak>".format(response_text)
+    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
     # Print the response
     print(response_text)
--- Labfiles/08-speech-translation/Python/translator/translator.py
+++ Labfiles/08-speech-translation/Python/translator/translator.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import os
 
 # Import namespaces
-
+import azure.cognitiveservices.speech as speech_sdk
+from playsound import playsound
 
 def main():
     try:
@@ -16,9 +17,16 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure translation
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(ai_key, ai_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        print('Ready to translate from',translation_config.speech_recognition_language)
 
 
         # Configure speech
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
 
 
         # Get user input
@@ -37,10 +45,37 @@ def main():
 def Translate(targetLanguage):
     translation = ''
 
-    # Translate speech
+    # Translate speech - microphone
+    # audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    # translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config = audio_config)
+    # print("Speak now...")
+    # result = translator.recognize_once_async().get()
+    # print('Translating "{}"'.format(result.text))
+    # translation = result.translations[targetLanguage]
+    # print(translation)
 
+    # Translate speech - audio file
+    audioFile = 'station.wav'
+    playsound(audioFile)
+    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config = audio_config)
+    print("Getting speech from file...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
 
     # Synthesize translation
+    voices = {
+            "fr": "fr-FR-HenriNeural",
+            "es": "es-ES-ElviraNeural",
+            "hi": "hi-IN-MadhurNeural"
+    }
+    speech_config.speech_synthesis_voice_name = voices.get(targetLanguage)
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+    speak = speech_synthesizer.speak_text_async(translation).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
 
 
